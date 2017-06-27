bd7df9721ec8b75adda32ae672106e4182dc74f5
Alexey Nikitin <nikialeksey@gmail.com>
2017-06-27 22:47:00 +0700

    Trying 0pdd

--- app/src/main/java/me/nikialeksey/fullscreendialog/MainActivity.java
+++ app/src/main/java/me/nikialeksey/fullscreendialog/MainActivity.java
@@ -8,8 +8,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // @todo #2:30min Remove xml layout
         setContentView(R.layout.activity_main);
 
-        // @todo #1:15min First issue
+        // @todo #1:15min Add full screen dialog showing
     }
 }
