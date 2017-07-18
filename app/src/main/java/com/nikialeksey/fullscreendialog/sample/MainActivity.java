9c9963a911888176c41a8ce70ca12de642b12a0d
Alexey Nikitin <a.nikitin@modulbank.ru>
2017-07-18 15:48:37 +0700

    Added status bar true color

--- app/src/main/java/com/nikialeksey/fullscreendialog/sample/MainActivity.java
+++ app/src/main/java/com/nikialeksey/fullscreendialog/sample/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,27 +20,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         final FrameLayout content = new FrameLayout(this);
+
         final TextView textView = new TextView(this, null,
                 android.R.style.TextAppearance_Material_Small);
         textView.setText(R.string.mainSampleText);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new FsDialog(MainActivity.this, R.style.AppTheme, getString(R.string.mainFsDialogTitle), new FsDialogCloseAction() {
+                    @Override
+                    public void onClose(@NonNull final FsDialog dialog) {
+
+                    }
+                }, "Action", new FsDialogAction() {
+                    @Override
+                    public void onAction(FsDialog dialog) {
+                        dialog.dismiss();
+                        Toast.makeText(MainActivity.this, "Sample action", Toast.LENGTH_LONG).show();
+                    }
+                }).show();
+            }
+        });
+
         final FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(
                 FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT);
         lp.gravity = Gravity.CENTER;
         content.addView(textView, lp);
 
         setContentView(content);
-
-        new FsDialog(this, getString(R.string.mainFsDialogTitle), new FsDialogCloseAction() {
-            @Override
-            public void onClose(@NonNull final FsDialog dialog) {
-
-            }
-        }, "Action", new FsDialogAction() {
-            @Override
-            public void onAction(FsDialog dialog) {
-                dialog.dismiss();
-                Toast.makeText(MainActivity.this, "Sample action", Toast.LENGTH_LONG).show();
-            }
-        }).show();
     }
 }
--- lib/src/main/java/com/nikialeksey/fullscreendialog/FsDialog.java
+++ lib/src/main/java/com/nikialeksey/fullscreendialog/FsDialog.java
@@ -4,8 +4,13 @@ package com.nikialeksey.fullscreendialog;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
+import android.support.annotation.AttrRes;
+import android.support.annotation.ColorInt;
 import android.support.annotation.NonNull;
+import android.support.annotation.StyleRes;
 import android.support.v7.app.AppCompatDialog;
+import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
@@ -13,16 +18,21 @@ import android.view.WindowManager;
 
 public class FsDialog extends AppCompatDialog {
 
-    public FsDialog(@NonNull final Context context, @NonNull final String title,
-                    @NonNull final FsDialogCloseAction closeAction,
+    public FsDialog(@NonNull final Context context, @StyleRes int appThemeId,
+                    @NonNull final String title, @NonNull final FsDialogCloseAction closeAction,
                     @NonNull final String actionTitle, @NonNull final FsDialogAction action) {
-        super(context);
-        // @todo #10:60m make the system bar and nav bar true color, when dialog is showing
+        super(context, appThemeId);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
 
         final Window window = getWindow();
         if (window == null) {
             throw new IllegalStateException("Non visual activity");
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(getThemeColor(getContext(), R.attr.colorPrimaryDark));
         }
 
         final View root = new FsDialogLayout(context, new FsDialogToolbar(context, title, new FsToolbarCloseAction() {
@@ -43,5 +53,13 @@ public class FsDialog extends AppCompatDialog {
         window.setGravity(Gravity.CENTER);
         window.setLayout(WindowManager.LayoutParams.MATCH_PARENT,
                 WindowManager.LayoutParams.MATCH_PARENT);
+    }
+
+    // @todo #11:30m change this method to class
+    @ColorInt
+    public int getThemeColor(@NonNull final Context context, @AttrRes final int attributeColor) {
+        final TypedValue value = new TypedValue();
+        context.getTheme().resolveAttribute(attributeColor, value, true);
+        return value.data;
     }
 }
