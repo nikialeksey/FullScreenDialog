3625fa3b1c634c2f5bb9f5bfb188330c98d3619d
Alexey Nikitin <a.nikitin@modulbank.ru>
2017-07-20 14:59:05 +0700

    Added empty espresso test

---
+++ lib/src/androidTest/java/com/nikialeksey/fullscreendialog/FsDialogTest.java
@@ -0,0 +1,10 @@
+package com.nikialeksey.fullscreendialog;
+
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class FsDialogTest {
+}
