2e4294570ffc1c4ef70f680d3cd7ed748ff55b41
Alexey Nikitin <nikialeksey@gmail.com>
2017-07-27 18:59:05 +0700

    Refactor

--- lib/src/androidTest/java/com/nikialeksey/fullscreendialog/FsDialogTest.java
+++ lib/src/androidTest/java/com/nikialeksey/fullscreendialog/FsDialogTest.java
@@ -1,23 +1,15 @@
 package com.nikialeksey.fullscreendialog;
 
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.test.espresso.Espresso;
-import android.support.test.espresso.action.ViewActions;
-import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import org.hamcrest.core.Is;
-import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
